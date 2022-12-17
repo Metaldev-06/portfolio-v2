4b96ede851a3715fa226f3c43ba98447fc661a6d
Fernando Diaz <70718713+Metaldev-06@users.noreply.github.com>
2022-12-17 19:01:05 -0300

    version 1.3.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio-v2';
+
+  ngOnInit() {
+    let msg = `%cBienvenido a mi Portfolio Personal`
+
+    let styles = [
+      'padding: 2rem 5rem',
+      'background: linear-gradient(to top right, #504DF9 0%, #E80397 61%);',
+      'color: white',
+      'display: block',
+      'text-align: center',
+      'font-weight: 900',
+      'font-size: 2rem',
+
+    ].join(';')
+    console.log(msg, styles);
+
+  }
+  
 }
